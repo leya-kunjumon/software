cefdff5e9a5b1a9775f14c2eb1718a429b9bbe02
Emil-20 <emilvarghese2000@gmail.com~>
2022-03-14 17:18:54 +0530

    developer

--- software/urls.py
+++ software/urls.py
@@ -1,9 +1,11 @@
-from django import views
+from app import views
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', views.index, name='index'),
+    # path('index', views.index, name='index'),
+    path('developer_Dashboard',views.developer_Dashboard, name='developer_Dashboard'),
     
 ]
