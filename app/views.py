8be8c05d7f1154fd79599d1258e15c1531737b0c
Emil-20 <emilvarghese2000@gmail.com~>
2022-03-14 23:40:07 +0530

    14Developercommite

--- app/views.py
+++ app/views.py
@@ -7,6 +7,38 @@ from django.shortcuts import render
 def developer_Dashboard(request):
     return render(request,'developer\developer_Dashboard.html')
 
+def developer_task(request):
+    return render(request,'developer\developer_task.html')
+
+def developer_task_form(request):
+    return render(request,'developer\developer_task_form.html')
+def developer_project(request):
+    return render(request,'developer\developer_project.html')
+def developer_project_table(request):
+    return render(request,'developer\developer_project_table.html')
+def developer_attendance(request):
+    return render(request,'developer\developer_attendance.html')
+def developer_attendance_list(request):
+    return render(request,'developer\developer_attendance_list.html')
+def developer_reportedissue(request):
+    return render(request,'developer\developer_reportedissue.html')
+def developer_reportedissues(request):
+    return render(request,'developer\developer_reportedissues.html')
+def developer_reportissue(request):
+    return render(request,'developer\developer_reportissue.html')
+
+def developer_applyleave(request):
+    return render(request,'developer\developer_applyleave.html')
+def developer_apply_leave(request):
+    return render(request,'developer\developer_apply_leave.html')    
+def developer_apply_leave_list(request):
+    return render(request,'developer\developer_apply_leave_list.html')
+
+def developer_account_settings(request):
+    return render(request,'developer\developer_account_settings.html')
+def developer_change_password(request):
+    return render(request,'developer\developer_change_password.html')
+
 
 
 
--- software/urls.py
+++ software/urls.py
@@ -9,8 +9,23 @@ urlpatterns = [
 
 #***********************developer***********************************************
 
-    path('developer_Dashboard', views.developer_Dashboard, name='developer_Dashboard'),
+    path('developer_Dashboard/', views.developer_Dashboard, name='developer_Dashboard'),
+    path('developer_task/', views.developer_task, name='developer_task'),
+    path('developer_task_form/', views.developer_task_form, name='developer_task_form'),
+    path('developer_project/', views.developer_project, name='developer_project'),
+    path('developer_project_table/', views.developer_project_table, name='developer_project_table'),
+    path('developer_attendance/', views.developer_attendance, name='developer_attendance'),
+    path('developer_attendance_list/', views.developer_attendance_list, name='developer_attendance_list'),
+    path('developer_reportedissue/', views.developer_reportedissue, name='developer_reportedissue'),
+    path('developer_reportedissues/', views.developer_reportedissues, name='developer_reportedissues'),
+    path('developer_reportissue/', views.developer_reportissue, name='developer_reportissue'),
 
+    path('developer_applyleave/', views.developer_applyleave, name='developer_applyleave'),
+    path('developer_apply_leave/', views.developer_apply_leave, name='developer_apply_leave'),    
+    path('developer_apply_leave_list/', views.developer_apply_leave_list, name='developer_apply_leave_list'),
+
+    path('developer_account_settings/', views.developer_account_settings, name='developer_account_settings'),
+    path('developer_change_password/', views.developer_change_password, name='developer_change_password'),
 
     
 
