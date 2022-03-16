fc3a4373ed641b1acccf0a2b1be5b877a2f2c581
Leya Kunjumon <leyakunjumon99@gmail.com>
2022-03-16 09:36:17 +0530

    leya final commit

--- app/views.py
+++ app/views.py
@@ -14,6 +14,27 @@ def index(request):
 def manager_Dashboard(request):
     return render(request,'manager\manager_Dashboard.html')
 
+######################### TESTER #################################
 
-
+def tester_dashboard(request):
+    return render(request,'tester/tester_dashboard.html')  
     
+
+def tester_task(request):
+    return render(request, 'tester/tester_task.html')
+
+
+def tester_taskassigned(request):
+    return render(request, 'tester/tester_taskassigned.html')
+
+
+def tester_taskpending(request):
+    return render(request, 'tester/tester_taskpending.html')
+    
+
+def tester_projects(request):
+    return render(request, 'tester/tester_projects.html')
+    
+
+def tester_projectdetails(request):
+    return render(request, 'tester/tester_projectdetails.html')
--- software/urls.py
+++ software/urls.py
@@ -12,5 +12,19 @@ urlpatterns = [
     #***************************** Manager ******************************
 
     path('manager_Dashboard',views.manager_Dashboard),
-    
+   
+   ############################# TESTER ################################
+   
+   path('tester_dashboard', views.tester_dashboard, name='tester_dashboard'), 
+   path('tester_task', views.tester_task, name='tester_task'), 
+   path('tester_taskassigned', views.tester_taskassigned,name='tester_taskassigned'),
+   path('tester_taskpending', views.tester_taskpending,name='tester_taskpending'),
+   path('tester_projects', views.tester_projects, name='tester_projects'),
+   path('tester_projectdetails', views.tester_projectdetails,name='tester_projectdetails'),
+   
+   
+   
+   
+
+   
 ]
